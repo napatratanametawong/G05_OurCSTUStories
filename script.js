7f1c189106b747f249ae5e55eb09814d177f7e98
unknown <napat.rata@dome.tu.ac.th>
2024-11-27 14:23:50 +0700

    make navbar change color when scroll down

---
+++ script.js
@@ -0,0 +1,30 @@
+//Homepage
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+//Journey
+
+
+
+
+
+
+
+
+
+//Future
